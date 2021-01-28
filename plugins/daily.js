96c707c4f7ab72d62e36f1c347dfdb156424816e
kelvin44 <66330608+kelvin44@users.noreply.github.com>
2021-01-28 21:56:10 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
-    conn.reply(m.chat, 'Selamat anda mendapatkan +9999 XP', m)  
-    global.DATABASE._data.users[m.sender].exp += 9999
+    conn.reply(m.chat, 'Selamat anda mendapatkan +500 XP', m)  
+    global.DATABASE._data.users[m.sender].exp += 500
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
   } else conn.reply(m.chat, 'Anda sudah mengklaim klaim harian  hari ini', m)
 }
